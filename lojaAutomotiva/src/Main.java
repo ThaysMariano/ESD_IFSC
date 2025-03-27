515be8ae193a08b6a49f152c37aa157faf82a9f9
thays <thaysdasilvamariano@gmail.com>
2025-03-27 17:27:10 -0300

    Atividade primiera aula

---
+++ lojaAutomotiva/src/Main.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayList<String> clientes = new ArrayList<>();
+
+        //para não iniciar com ele vazio apenas
+        clientes.add("thays");
+        clientes.add("kauan");
+
+        boolean sair = false;
+
+        do {
+            System.out.print(">");
+            String entrada = scanner.nextLine();
+
+            switch (entrada) {
+                case "?":   //apresentar nome do prox cliente e remover da lista
+                    System.out.println(clientes.remove(0));
+                    break;
+                case "sair":    //terminar o programa
+                    sair = true;
+                    break;
+                default:        //quantos clientes na frente e adicionar a fila
+                    clientes.add(entrada);
+                    System.out.println(clientes.indexOf(entrada));
+            }
+        }while (!sair);
+    }
+}
