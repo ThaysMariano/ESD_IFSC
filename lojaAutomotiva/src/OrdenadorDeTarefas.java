1ece626b1817312211e45f8f286ed262b01eb6a5
thays <thaysdasilvamariano@gmail.com>
2025-03-28 17:08:27 -0300

    Arquivo para desafio de Organizador de Tarefas

---
+++ lojaAutomotiva/src/OrdenadorDeTarefas.java
@@ -0,0 +1,43 @@
+public class OrdenadorDeTarefas {
+
+    public static void main(String[] args) {
+
+        /*
+        * Um arquivo contém uma listagem das tarefas de um projeto, as quais possuem uma
+        * prioridade. Cada linha desse arquivo se apresenta desta forma:
+        *prioridade: tarefa
+        *
+        * Escreva um programa que leia um arquivo desses e apresente as tarefas ordenadas por
+        * suas prioridades (valores menores significam maiores prioridades).  O nome do arquivo é
+        * informado no primeiro argumento de linha de comando
+        *
+        * TODO conferir comando de leitura de terminal
+        *
+        * Observe que as tarefas de mesma prioridade devem estar na mesma ordem que no arquivo original.
+        * Sabendo que existem, no máximo, 5 níveis de prioridade (de 1 até 5), implemente o programa
+        * que faz esse ordenamento.
+        *
+        */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+}
